ba2093dee67980852ba9b6aeff2cc91b6bf8736c
Tuan-Anh Nguyen <ta.nguyen1893@gmail.com>
2016-10-13 19:49:42 -0400

    refactor to C coding style

--- pooling.c
+++ pooling.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "omp.h"
- #include <math.h>
-
+#include <math.h>
 
 //	#define max(a,b) ((a) > (b) ? (a) : (b))
 
@@ -22,57 +21,59 @@ int max(int num1, int num2)
 	return result;
 }
 
-
-
-void pool(char* input_filename, char* output_filename,char* threads)
+void pool(char* input_filename, char* output_filename, char* threads)
 {
-  unsigned error;
-  unsigned char *image, *new_image;
-  unsigned width, height;
+	unsigned error;
+	unsigned char *image, *new_image;
+	unsigned width, height;
 
+	error = lodepng_decode32_file(&image, &width, &height, input_filename);
 
-  error = lodepng_decode32_file(&image, &width, &height, input_filename);
+	if (error)
+		printf("error %u: %s\n", error, lodepng_error_text(error));
+	new_image = malloc(width * height * sizeof(unsigned char));
 
-  if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
-  new_image = malloc(width * height * sizeof(unsigned char));
+	// process image
+	unsigned char value;
 
-  // process image
-  unsigned char value;
-
-omp_set_num_threads(atoi(threads));
+	omp_set_num_threads(atoi(threads));
 #pragma omp parallel for collapse(2)
-  for (int i = 0; i < height; i+=2) {
-    for (int j = 0; j < width; j+=2) {
-    	// get all adjacent squares
-    	int main = 4*width*i + 4*j;
-    	int down = 4*width*(i+1) + 4*j;
-		int right = 4*width*i + 4*(j+1);
-		int diagonal = 4*width*(i+1) + 4*(j+1);
+	for (int i = 0; i < height; i += 2)
+	{
+		for (int j = 0; j < width; j += 2)
+		{
+			// get all adjacent squares
+			int main = 4 * width * i + 4 * j;
+			int down = 4 * width * (i + 1) + 4 * j;
+			int right = 4 * width * i + 4 * (j + 1);
+			int diagonal = 4 * width * (i + 1) + 4 * (j + 1);
 
-		int maxR = max( max(image[main], image[down]), max(image[right], image[diagonal]));
-		int maxG = max( max(image[main+1], image[down+1]), max(image[right+1], image[diagonal+1]));
-		int maxB = max( max(image[main+2], image[down+2]), max(image[right+2], image[diagonal+2] ));
+			int maxR = max(max(image[main], image[down]),
+					max(image[right], image[diagonal]));
+			int maxG = max(max(image[main + 1], image[down + 1]),
+					max(image[right + 1], image[diagonal + 1]));
+			int maxB = max(max(image[main + 2], image[down + 2]),
+					max(image[right + 2], image[diagonal + 2]));
 
-		new_image[width*i + j*2] = maxR;
-		new_image[(width*i + j*2) + 1] = maxG;
-		new_image[(width*i + j*2) + 2] = maxB;
-		new_image[(width*i + j*2) + 3] = 255;
-    }
-  }
+			new_image[width * i + j * 2] = maxR;
+			new_image[(width * i + j * 2) + 1] = maxG;
+			new_image[(width * i + j * 2) + 2] = maxB;
+			new_image[(width * i + j * 2) + 3] = 255;
+		}
+	}
 
+	lodepng_encode32_file(output_filename, new_image, width / 2, height / 2);
 
-  lodepng_encode32_file(output_filename, new_image, width/2, height/2);
-
-  free(image);
-  free(new_image);
+	free(image);
+	free(new_image);
 }
 
 int main(int argc, char *argv[])
 {
-  char* input_filename = argv[1];
-  char* output_filename = argv[2];
+	char* input_filename = argv[1];
+	char* output_filename = argv[2];
 
-  pool(input_filename, output_filename,argv[3]);
+	pool(input_filename, output_filename, argv[3]);
 
-  return 0;
+	return 0;
 }
--- q1.c
+++ q1.c
@@ -9,64 +9,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "omp.h"
- #include <math.h>
+#include <math.h>
 
-
-void rec(int pixel,int address,unsigned char * new_image) {
-	if (pixel<127) {
-		new_image[address]=127;
+void rec(int pixel, int address, unsigned char * new_image)
+{
+	if (pixel < 127)
+	{
+		new_image[address] = 127;
 	}
-	else{
-		new_image[address]=pixel;
+	else
+	{
+		new_image[address] = pixel;
 	}
 }
 
-
-void process(char* input_filename, char* output_filename,char* threads)
+void process(char* input_filename, char* output_filename, char* threads)
 {
-  unsigned error;
-  unsigned char *image, *new_image;
-  unsigned width, height;
+	unsigned error;
+	unsigned char *image, *new_image;
+	unsigned width, height;
 
-  error = lodepng_decode32_file(&image, &width, &height, input_filename);
-  if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
-  new_image = malloc(width * height * 4 * sizeof(unsigned char));
+	error = lodepng_decode32_file(&image, &width, &height, input_filename);
+	if (error)
+		printf("error %u: %s\n", error, lodepng_error_text(error));
+	new_image = malloc(width * height * 4 * sizeof(unsigned char));
 
-  // process image
-  unsigned char value;
-  int xyz = 123; // more computations
-omp_set_num_threads(atoi(threads));
+	// process image
+	unsigned char value;
+	int xyz = 123; // more computations
+	omp_set_num_threads(atoi(threads));
 #pragma omp parallel for collapse(2)
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-    	// printf("thread num: %d\n", omp_get_num_threads());
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			// printf("thread num: %d\n", omp_get_num_threads());
 
-    	// printf("thread num: %d\n", omp_get_thread_num());
-    	int offset = 4*width*i + 4*j;
-	    int r =image[offset + 0]; // R
-	    rec(r,(offset + 0),new_image);
-	    int g =image[offset + 1]; // G
-	    rec(g,offset+1,new_image);
-	    int b =image[offset + 2]; // B
-	    rec(b,offset+2,new_image);
-	    int a = image[offset + 3]; // A
-	    new_image[offset + 3]=a;
-	    xyz = pow(sin(i+xyz), cos(xyz + j));
-    }
-  }
+			// printf("thread num: %d\n", omp_get_thread_num());
+			int offset = 4 * width * i + 4 * j;
+			int r = image[offset + 0]; // R
+			rec(r, (offset + 0), new_image);
+			int g = image[offset + 1]; // G
+			rec(g, offset + 1, new_image);
+			int b = image[offset + 2]; // B
+			rec(b, offset + 2, new_image);
+			int a = image[offset + 3]; // A
+			new_image[offset + 3] = a;
+			xyz = pow(sin(i + xyz), cos(xyz + j));
+		}
+	}
 
-  lodepng_encode32_file(output_filename, new_image, width, height);
+	lodepng_encode32_file(output_filename, new_image, width, height);
 
-  free(image);
-  free(new_image);
+	free(image);
+	free(new_image);
 }
 
 int main(int argc, char *argv[])
 {
-  char* input_filename = argv[1];
-  char* output_filename = argv[2];
+	char* input_filename = argv[1];
+	char* output_filename = argv[2];
 
-  process(input_filename, output_filename,argv[3]);
+	process(input_filename, output_filename, argv[3]);
 
-  return 0;
+	return 0;
 }
